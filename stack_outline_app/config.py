abeec8b1906defc0a9c34d8d53cec39a39f8ca70
materialsteam13 <materialsteam13@gmail.com>
2026-05-15 14:35:34 -0500

    Refactor database URI for relative pathing

--- stack_outline_app/config.py
+++ stack_outline_app/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
     # Using the exact path from your debug log + the filename
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/ashar/Desktop/CodingSoftware/IntroDatabases/katchup.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'katchup.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
